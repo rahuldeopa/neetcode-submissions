6a703c2082860c1d6cc5ea0e250cda47e1a88645
RAHUL DEOPA <40573170+rahuldeopa@users.noreply.github.com>
2026-06-26 18:50:00 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder st=new StringBuilder();
+
+        int l=s.length();
+
+        for(int i=0;i<l;i++)
+        {
+            if ((s.charAt(i) < 'A' || s.charAt(i) > 'Z') && (s.charAt(i) < 'a' || s.charAt(i) > 'z') && (s.charAt(i)<'0' || s.charAt(i)>'9'))
+            {
+                continue;
+            }
+            st.append(s.charAt(i));
+        }
+        String str=st.toString().toLowerCase();
+        int j=0;
+        int k=st.length()-1;
+        System.out.println(str);
+        while(j<=k)
+        {
+            if(str.charAt(j)!=str.charAt(k))
+            {
+                return false;
+            }
+            j++;
+            k--;
+        }
+        return true;
+    }
+}
